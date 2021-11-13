b972359915108158e779cc04a90a1e7f8e65a2cb
wwplaymc1207 <92558506+wwplaymc1207@users.noreply.github.com>
2021-11-13 08:23:42 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,20 @@
+import discord
+from discord.ext import commands
 
+bot = commands.Bot(command_prefix='w!')
+
+@bot.event
+async def on_ready():
+    print(">> Bot is online <<")
+
+@bot.event
+async def on_member_join(member):
+    channel = bot.get_channel(905220048090714162)
+    await channel.send(f'{member} 欢迎加入FBSMP!')
+
+@bot.event
+async def on_member_remove(member):
+    channel = bot.get_channel(905220048090714162)
+    await channel.send(f'{member} 离开了FBSMP!qwq')
+
+bot.run('OTAyNDI1MDQ5MTA4MTkzMjgw.YXeO0Q.RByYawRkHoRPh1oC5aKdnvs4wDA')
