9ad4a679e6820a5f49cfade03bec0b9389ba3b5b
wwplaymc1207 <92558506+wwplaymc1207@users.noreply.github.com>
2021-11-13 15:19:32 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,65 @@
 import discord
 from discord.ext import commands
 import json
+import random
+import time
+import os
+
+with open('setting.json', 'r', encoding='utf8') as jfile:
+    jdata = json.load(jfile)
 
 bot = commands.Bot(command_prefix='w!')
-
-with open('setting.json', 'r', encoding='utf-8') as jfile:
-    jdata=json.load(jfile)
 
 @bot.event
 async def on_ready():
     print(">> Bot is online <<")
+    await bot.change_presence(activity=discord.Game(name=f'w!help | 已被邀请至 {len(bot.guilds)} 个服务器'))
+
+bot.remove_command('help')
+@bot.command()
+async def help(ctx):
+    help = discord.Embed(title="指令说明", color=0xffff8a)
+    help.set_author(name="凋凋的可爱机器人", icon_url="https://cdn.discordapp.com/avatars/902425049108193280/d7fdedddf2b86e74ccdab706940d0b77.png?size=80")
+    help.add_field(name="Help \nAdhelp \nPing \nDice", value="** **")
+    await ctx.send(embed=help)
+
+@bot.command()
+async def load(self, ctx, extension):
+    myID = 826714957517291552
+    if ctx.author.id == myID:
+        self.bot.load_extension(f'cmds.{extension}')
+        await ctx.send(f"{extension} 已加载完毕哥哥~")
+
+@bot.command()
+async def unload(self, ctx, extension):
+    myID = 826714957517291552
+    if ctx.author.id == myID:
+        self.bot.unload_extension(f'cmds.{extension}')
+        await ctx.send(f"{extension} 已取消加载完毕哥哥~")
+
+@bot.command()
+async def reload(self, ctx, extension):
+    myID = 826714957517291552
+    if ctx.author.id == myID:
+        self.bot.reload_extension(f'cmds.{extension}')
+        await ctx.send(f"{extension} 已重新加载完毕哥哥~")
 
 @bot.event
-async def on_member_join(member):
-    channel = bot.get_channel(905220048090714162)
-    await channel.send(f'{member} 欢迎加入FBSMP!')
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.MissingRequiredArgument):
+        await ctx.send("你没输入参数哦哥哥~")
+    elif isinstance(error, commands.errors.CommandNotFound):
+        await ctx.send("没有这个指令哦哥哥~")
+    elif isinstance(error, commands.errors.MissingPermissions):
+        await ctx.send("你没权限使用这个指令哦哥哥~")
+    elif isinstance(error, commands.errors.MissingRole):
+        await ctx.send("你没指定身分组使用这个指令哦哥哥~")
+    else:
+        await ctx.send("完蛋了系统发生错误qwq")
+        
+for filename in os.listdir('./cmds'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cmds.{filename[:-3]}')
 
-@bot.event
-async def on_member_remove(member):
-    channel = bot.get_channel(905220048090714162)
-    await channel.send(f'{member} 离开了FBSMP!qwq')
-bot.run(jdata['token'])
+if __name__ == "__main__":
+    bot.run(jdata['TOKEN'])
