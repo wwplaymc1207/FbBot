d6f8262eef14b777d4d3b34ef4d4a1bf897934e3
wwplaymc1207 <92558506+wwplaymc1207@users.noreply.github.com>
2021-11-13 08:45:23 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,11 @@
 import discord
 from discord.ext import commands
+import json
 
 bot = commands.Bot(command_prefix='w!')
+
+with open('setting.json', 'r', encoding='utf-8') as jfile:
+    jdata=json.load(jfile)
 
 @bot.event
 async def on_ready():
@@ -16,5 +20,4 @@ async def on_member_join(member):
 async def on_member_remove(member):
     channel = bot.get_channel(905220048090714162)
     await channel.send(f'{member} 离开了FBSMP!qwq')
-
-bot.run('OTAyNDI1MDQ5MTA4MTkzMjgw.YXeO0Q.RByYawRkHoRPh1oC5aKdnvs4wDA')
+bot.run(jdata['token'])
